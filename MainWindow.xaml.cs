77baeb875a4ed21aaed008b73abc30b40297a7ec
Lucas <jammes.lucas@proton.me>
2025-04-27 20:32:00 +0200

    feat: right click to set dead state, fix refresh button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,53 +15,66 @@ namespace AmongUs_Helper
             InitializeComponent();
         }
 
-        // Drag Window by clicking on the top bar
-        private void TopBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Character_LeftClick(object sender, MouseButtonEventArgs e)
         {
-            DragMove();
+            if (sender is Image img && FindName($"{img.Name}_Label") is Label associatedLabel)
+            {
+                int state = img.Tag is int tag ? tag : 0;
+
+                switch (state)
+                {
+                    case 0:
+                        associatedLabel.Foreground = Brushes.LimeGreen;
+                        associatedLabel.Content = "SAFE";
+                        break;
+                    case 1:
+                        associatedLabel.Foreground = Brushes.Red;
+                        associatedLabel.Content = "SUS";
+                        break;
+                    case 2:
+                        associatedLabel.Foreground = Brushes.White;
+                        associatedLabel.Content = associatedLabel.Name.Replace("_Label", "");
+                        break;
+                }
+
+                img.Tag = (state + 1) % 3;
+            }
         }
 
-        private void Character_Click(object sender, MouseButtonEventArgs e)
+        private void Character_RightClick(object sender, MouseButtonEventArgs e)
         {
-            if (sender is Image img && img.Tag != null)
+            if (sender is Image img)
             {
-                // Get the associated label name
-                string labelName = $"{img.Name}_Label";
-
-                // Find the label in the window
-                if (FindName(labelName) is Label associatedLabel)
+                if (FindName($"{img.Name}_Label") is Label associatedLabel)
                 {
-                    // Get current state of the image
-                    if (int.TryParse(img.Tag.ToString(), out int state))
-                    {
-                        // Update the image source based on the new state
-                        switch (state)
-                        {
-                            case 0:
-                                associatedLabel.Foreground = Brushes.LimeGreen;
-                                break;
-                            case 1:
-                                associatedLabel.Foreground = Brushes.Red;
-                                break;
-                            case 2:
-                                associatedLabel.Foreground = Brushes.DarkSlateGray;
-                                break;
-                            case 3:
-                                associatedLabel.Foreground = Brushes.White;
-                                break;
-                        }
+                    bool isAlive = img.Opacity == 1.0;
 
-                        // Increment the state
-                        state = (state + 1) % 4;
-                        img.Tag = state;
-                    }
-                    else
-                    {
-                        _ = MessageBox.Show("Invalid state value.");
-                    }
+                    img.Opacity = isAlive ? 0.15 : 1.0;
+                    associatedLabel.Foreground = isAlive ? Brushes.DarkSlateGray : Brushes.White;
+                    associatedLabel.Content = isAlive ? "DEAD" : associatedLabel.Name.Replace("_Label", "");
                 }
             }
         }
+
+        private void Refresh_Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            foreach (object element in MainGrid.Children)
+            {
+                // Reset labels to default state
+                if (element is Label label && label.Name != "WindowTitle")
+                {
+                    label.Foreground = Brushes.White;
+                    label.Content = label.Name.Replace("_Label", "");
+                }
+                // Reset character images to default state
+                if (element is Border border && border.Child is Image img)
+                {
+                    img.Tag = 0;
+                    img.Opacity = 1.0;
+                }
+            }
+        }
+
 
         // Close when close_button is clicked
         private void Close_Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -69,18 +82,10 @@ namespace AmongUs_Helper
             Close();
         }
 
-        private void Refresh_Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        // Drag Window by clicking on the top bar
+        private void TopBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // Reset all labels to their default state
-            foreach (object? element in MainGrid.Children)
-            {
-                if (element is Label label && label.Name != "WindowTitle")
-                {
-                    // Reset the label's foreground color and content
-                    label.Foreground = Brushes.White;
-                    label.Content = label.Name.Replace("_Label", "");
-                }
-            }
+            DragMove();
         }
     }
 }
